c701393ed1ae97511a78525faced0f74e9ad1fbb
Bsaaristo <92605542+Bsaaristo@users.noreply.github.com>
2022-07-05 16:07:29 -0500

    Update Saaristo-Asg10-Single-Table.sql

--- Saaristo-Asg10-Single-Table.sql
+++ Saaristo-Asg10-Single-Table.sql
@@ -1,25 +1,25 @@
 
---ASG 10,  #1
+--  #1
   use MyGuitarShop
   select ProductCode, ProductName, ListPrice, DiscountPercent
   from Products
   ORDER BY ListPrice desc
 
-  -- ASG 10,  #2
+  --   #2
   use MyGuitarShop
   SELECT  lastname + ', ' + firstname as FullName
   FROM Customers
   where lastname >= 'M' and lastname <> 'Z'
   order by lastname
 
-  --ASG 10,     #3
+  --    #3
   use MyGuitarShop
   SELECT ProductName, ListPrice, DateAdded, ProductCode
   from Products
   where listprice >= 500 and listprice <= 2000
   order by dateadded desc
 
-  --ASG 10,    #4
+  --    #4
   use MyGuitarShop
   SELECT DiscountPercent / 100 * listprice as [DiscountAmmount]
   ,[ListPrice],[ProductName] 
@@ -28,7 +28,7 @@
   FROM Products
   order by discountprice asc
 
-   --ASG 10,     #5
+   --   #5
 
    use MyGuitarShop
    SELECT itemprice * quantity as [PriceTotal]
@@ -42,14 +42,14 @@
    where  ItemPrice - DiscountAmount * Quantity >= 300 
    order by itemtotal desc
 
-   --ASG 10,   #6
+   --   #6
 
    use MyGuitarShop
    select OrderID, OrderDate, ShipDate, ShipAmount
    from Orders
    where ShipDate is null
 
-   --ASG 10,   #7
+   --  #7
 
    use MyGuitarShop
    select Price = 100, TaxRate = .07, TaxAmount = 100 * .07, TotalSale = 100 + 7
